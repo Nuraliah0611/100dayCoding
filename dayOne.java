f40a85921e335b257eafa53c83b90739bb4a01d1
Nuraliah0611 <100122477+Nuraliah0611@users.noreply.github.com>
2023-10-09 20:20:22 +0700

    Add files via upload

---
+++ dayOne.java
@@ -0,0 +1,36 @@
+package javaapplication2;
+public class JavaApplication2 {
+    public static void main(String[] args) {
+        String nama = "Nejlah";  
+        System.out.println("nama = "+nama);
+        
+        byte jumlah_motor = 2;
+        System.out.println("jumlah motor = "+jumlah_motor);
+        
+        int ukuran_sepatu = 37;
+        System.out.println("ukuran sepatu = "+ukuran_sepatu);
+        
+        short angka_favorit = 4;
+        System.out.println("angka favorit = "+angka_favorit);
+        
+        boolean suka_olahraga = false;
+        System.out.println("suka olahraga = "+suka_olahraga);
+        
+        long tahun_lahir = 2003;
+        System.out.println("tahun lahir = "+tahun_lahir);
+        
+        char abjad_awal = 'a';
+        System.out.println("abjad awal = "+abjad_awal);
+        
+        double nilai_ip = 3.25f;
+        System.out.println("nilai ip = "+nilai_ip);
+        
+        float nilai_rata_rata = 7.5f;
+        System.out.println("nilai rata rata = "+nilai_rata_rata);
+        
+       
+        
+       
+    }
+    
+}
